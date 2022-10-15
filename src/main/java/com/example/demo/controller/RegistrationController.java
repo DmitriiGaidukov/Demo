5dcd898a8fb293b0c95d594e1403b009caf539ef
Dmitrij Gaydukov <dgaidukov@gmail.com>
2022-10-15 16:38:27 +0300

    validation

--- src/main/java/com/example/demo/controller/RegistrationController.java
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -17,8 +19,14 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("registration") Registration registration) {
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration registration,
+                                  BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
         System.out.println("Registration: " + registration.getName());
-        return "registration";
+        return "redirect:registration";
     }
 }
